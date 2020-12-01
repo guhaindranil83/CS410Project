63654060f1b7596d5f945b41c2f47f3325994a2b
zrupp <zachariarupp@gmail.com>
2020-11-30 16:48:29 -0800

    Add docstrings to miner.py

--- ExpertSearch/data/expertsearch/miner.py
+++ ExpertSearch/data/expertsearch/miner.py
@@ -15,14 +15,29 @@ CORPUS_DICTIONARY_FILE = '/data/expertsearch/corpus_dictionary'
 BIOS_FILE = '/data/compiled_bios'
 MODEL_FILES = '/data/expertsearch/model_files/'
 
+
 def get_topics_from_many_documents(doc_names):
+    """ A function that extracts a list of terms associated with each document's inferred topic.
 
+    Parameters
+    ----------
+    doc_names: list
+        A list of document filenames
+    """
     for doc_name in doc_names:
-        topic = get_topic_from_single_document(doc_name)
+        relevant_terms = get_topic_from_single_document(doc_name)
 
-        yield topic
+        yield relevant_terms
 
 def get_topic_from_single_document(doc_name):
+    """ A function that extracts a list of terms associated with a document's inferred topic.
+
+    Parameters
+    ----------
+    doc_name: str
+        The document's filename
+    """
+
     current_dir = os.getcwd()
 
     doc = list(iter_documents(current_dir + BIOS_FILE, doc_name).next())
@@ -35,6 +50,14 @@ def get_topic_from_single_document(doc_name):
     return clean_topic_terms(model.show_topic(topic_num))
 
 def get_top_words_from_query_topic(query):
+    """ A function that extracts a list of terms associated with a query's inferred topic.
+
+    Parameters
+    ----------
+    query: str
+        The query
+    """
+
     current_dir = os.getcwd()
     
     model = get_model()
@@ -49,10 +72,26 @@ def get_top_words_from_query_topic(query):
     yield top_terms
 
 def clean_topic_terms(topic):
+    """ A function that replaces '_' with ' ' for each term in a list of top-10 terms associated with a topic cluster.
+
+    Parameters
+    ----------
+    topic: list
+        The top-10 terms associated with a topic
+    """
     return[termscore[0].replace('_', ' ') for termscore in topic]
 
 def iter_documents(top_directory, doc_name):
-    """Iterate over all documents, yielding a document (=list of utf8 tokens) at a time."""
+    """ Seeks specified document in top_directory and processes it to yield a list of utf-8 tokens.
+
+    Parameters
+    ----------
+    top_directory: str
+        Path to folder containing documents
+
+    doc_name: str
+        Filename for document
+    """
     for root, dirs, files in os.walk(top_directory):
         for file in filter(lambda file: file == doc_name, files):
             document = open(os.path.join(root, file)).read().lower()  # read the entire document, as one big string
@@ -64,12 +103,20 @@ def iter_documents(top_directory, doc_name):
             yield document
 
 def get_model():
+    """ Gets a loaded topic model. """
     current_dir = os.getcwd()
     model = load_topic_model(current_dir + TOPIC_MODEL_FILE)
     model.prefix = current_dir + MODEL_FILES
     return model
 
 def load_topic_model(file_name):
+    """ Attempts to load a topic model from disk.
+
+    Parameters
+    ----------
+    file_name: str
+        Name of the file that holds the topic model
+    """
     try :
         return gensim.utils.SaveLoad.load(file_name)
     except IOError as err:
@@ -78,6 +125,14 @@ def load_topic_model(file_name):
         print("Something went wrong:", sys.exc_info()[0])
 
 def load_dictionary(file_name):
+    """ Attempts to load a gensim dictionary from disk.
+
+    Parameters
+    ----------
+    file_name: str
+        Name of the file that holds the dictionary
+    """
+
     try:
         return gensim.utils.SaveLoad.load(file_name)
     except IOError as err:
