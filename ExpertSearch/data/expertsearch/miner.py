0f354fb5456af8ce434ec4eb6ced507b588d967e
Zacharia Rupp <33986455+ZRupp@users.noreply.github.com>
2020-12-12 23:04:07 -0800

    Speed up miner.py and improve movel

--- ExpertSearch/data/expertsearch/miner.py
+++ ExpertSearch/data/expertsearch/miner.py
@@ -5,15 +5,17 @@ import cchardet
 import numpy as np
 from collections import OrderedDict
 import gensim
+import gensim.corpora as corpora
 from ranker import load_ranker
 from bs4 import BeautifulSoup
 
 
-
+CORPUS_FILE = '/data/expertsearch/lda_corpus'
 TOPIC_MODEL_FILE = '/data/expertsearch/lda_mallet_model'
 CORPUS_DICTIONARY_FILE = '/data/expertsearch/corpus_dictionary'
 BIOS_FILE = '/data/compiled_bios'
 MODEL_FILES = '/data/expertsearch/model_files/'
+INFERENCES = '/data/expertsearch/model_files/doctopics.txt'
 
 
 def get_topics_from_many_documents(doc_names):
@@ -24,10 +26,11 @@ def get_topics_from_many_documents(doc_names):
     doc_names: list
         A list of document filenames
     """
+    relevant_terms = []
     for doc_name in doc_names:
-        relevant_terms = get_topic_from_single_document(doc_name)
+        relevant_terms.append(get_topic_from_single_document(doc_name))
 
-        yield relevant_terms
+    return relevant_terms
 
 def get_topic_from_single_document(doc_name):
     """ A function that extracts a list of terms associated with a document's inferred topic.
@@ -39,13 +42,24 @@ def get_topic_from_single_document(doc_name):
     """
 
     current_dir = os.getcwd()
-
-    doc = list(iter_documents(current_dir + BIOS_FILE, doc_name).next())
-    corpus_dictionary = load_dictionary(current_dir + CORPUS_DICTIONARY_FILE)
     model = get_model()
+    corpus = corpora.MmCorpus(current_dir + CORPUS_FILE)
+    doc_id = int(doc_name.replace('.txt', ''))
+    print(doc_id)
+    print(doc_id >= len(corpus))
+    if doc_id >= len(corpus): # Do inference on new document
 
-    topic_num = max(model[corpus_dictionary.doc2bow(doc)], key=lambda x: x[1])[0]
+        doc = get_document(current_dir + BIOS_FILE, doc_name)
+        corpus_dictionary = load_dictionary(current_dir + CORPUS_DICTIONARY_FILE)
 
+        topic_num = max(model[corpus_dictionary.doc2bow(doc)], key=lambda x: x[1])[0]
+    else: 
+
+        with open(current_dir + INFERENCES) as f:
+            for i, line in enumerate(f):
+                if i == doc_id:
+                    topic_distribution = list(map(float,line.replace('\n','').split('\t')[2:]))
+                    topic_num = topic_distribution.index(max(topic_distribution))
 
     return clean_topic_terms(model.show_topic(topic_num))
 
@@ -61,6 +75,7 @@ def get_top_words_from_query_topic(query):
     current_dir = os.getcwd()
     
     model = get_model()
+
     corpus_dictionary = load_dictionary(current_dir + CORPUS_DICTIONARY_FILE)
 
     split_query = query.split(' ')
@@ -81,8 +96,8 @@ def clean_topic_terms(topic):
     """
     return[termscore[0].replace('_', ' ') for termscore in topic]
 
-def iter_documents(top_directory, doc_name):
-    """ Seeks specified document in top_directory and processes it to yield a list of utf-8 tokens.
+def get_document(top_directory, doc_name):
+    """ Processes specified document to yield a list of utf-8 tokens.
 
     Parameters
     ----------
@@ -92,15 +107,17 @@ def iter_documents(top_directory, doc_name):
     doc_name: str
         Filename for document
     """
-    for root, dirs, files in os.walk(top_directory):
-        for file in filter(lambda file: file == doc_name, files):
-            document = open(os.path.join(root, file)).read().lower()  # read the entire document, as one big string
-            soup = BeautifulSoup(document, "lxml")
-            document = soup.get_text(separator='\n').split(' ')
-            document = gensim.utils.simple_preprocess(' '.join(document))
-            document = [word for word in document if len(word) > 4]
 
-            yield document
+    fp = open(os.path.join(top_dir, file))
+    document = fp.read().lower()  # read the entire document, as one big string
+    
+    #soup = BeautifulSoup(document, "lxml")
+    #document = soup.get_text(separator='\n').split(' ')
+    document = gensim.utils.simple_preprocess(' '.join(document))
+    document = [word for word in document if len(word) > 4]
+
+    fp.close()
+    return document
 
 def get_model():
     """ Gets a loaded topic model. """
